08d7a117390f29dfd33adab1c29a3b19d4741463
Prajan53 <sprajan53@gmail.com>
2025-03-25 15:48:07 +0530

    fix: type errors

--- src/store/useJobStore.tsx
+++ src/store/useJobStore.tsx
@@ -1,16 +1,11 @@
 import { create } from "zustand";
 
 interface JobStoreState {
-    //@ts-ignore
   jobs: any[];
-  //@ts-ignore
   filteredJobs: any[];
-
   searchQuery: string;
   selectedLocation: string;
-  //@ts-ignore
   selectedJobType: string;
-  //@ts-ignore
   salaryRange: [number, number];
 
   setJobs: (jobs: any[]) => void;
